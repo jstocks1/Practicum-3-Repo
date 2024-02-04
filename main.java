57d90a76b84086c967307849f1598dda7bd337f3
jstocks1 <jstocks1@students.towson.edu>
2024-02-04 12:44:16 -0500

    I changed the identifier names so that the program would run.

--- main.java
+++ main.java
@@ -1,15 +1,15 @@
 // Identifier Naming in Java
-
-public class Main {
+// Jackson Stockstill
+public class main {
   
   public static void main(String[] args) {
-    int 1st_num = 1;
+    int num_1 = 1;
     int last_num = 10;
     int Sum = 0;
 
-    for(int i = 1st_num; i <= last_num; i++)
-      sum = sum + i;
+    for(int i = num_1; i <= last_num; i++)
+      Sum = Sum + i;
   
-    System.out.println("Sum = " + sum);
+    System.out.println("Sum = " + Sum);
   }
 }
